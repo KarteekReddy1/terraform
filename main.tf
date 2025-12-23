7fc71bfedd1eadf4a9453a163e6187cb3b0824b1
KarteekReddy1 <kalluri.gt@gmail.com>
2025-12-23 23:52:21 +0530

    terraform commit

--- main.tf
+++ main.tf
@@ -2,9 +2,18 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
+  subnet_id     = "data.aws_subnet.default.ids"
 
   tags = {
     Name = "jenkins-terraform-ec2"
