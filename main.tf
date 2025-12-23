b254a85264e2c4276c43248b6d58637bad0be114
KarteekReddy1 <kalluri.gt@gmail.com>
2025-12-23 23:57:46 +0530

    terraform commit

--- main.tf
+++ main.tf
@@ -6,14 +6,17 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet" "default" {
-  vpc_id = data.aws_vpc.default.id
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
-  subnet_id     = "data.aws_subnet.default.ids"
+  subnet_id     = data.aws_subnets.default.ids[0]   # pick the first subnet
 
   tags = {
     Name = "jenkins-terraform-ec2"
